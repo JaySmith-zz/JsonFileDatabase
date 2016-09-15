85f088ffcbdcaa5cf59e3e76fa992c16eb21dcc5
Smith <jay.w.smith@tyson.com>
2016-09-15 09:16:06 -0500

    initial

---
+++ source/JsonFileDatabase.Tests/TestClass.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magrathea.Data;
+
+namespace JsonFileDatabase.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+
+
+
+        public void ReadFormatedFile()
+        {
+            var dataStore = new Magrathea.Data.FileDataRepository(@"C:\Temp\testdata.json");
+            var data = dataStore.Read<UserDatabase>();
+        }
+
+        [Test]
+        public void WritesFormatedFile()
+        {
+            var data = new UserDatabase();
+
+            data.Users.Add(new User
+            {
+                UserName = "user1",
+                Password = "pass1"
+            });
+
+            data.Users.Add(new User
+            {
+                UserName = "user2",
+                Password = "pass2"
+            });
+
+            var dataStore = new FileDataRepository(@"C:\Temp\testdata.json");
+            dataStore.Save(data);
+
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+
+    class UserDatabase
+    {
+        public UserDatabase()
+        {
+            Users = new List<User>();
+        }
+
+        public List<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ source/JsonFileDatabase/FileDataRepository.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Magrathea.Data
+{
+    /// <summary>
+    /// Generic File Data Repository
+    /// </summary>
+    public class FileDataRepository
+    {
+        private string _filename;
+
+        /// <summary>
+        /// Create a new instance of the FileDataRepository
+        /// </summary>
+        /// <param name="fileName"></param>
+        public FileDataRepository(string fileName)
+        {
+            _filename = fileName;
+        }
+
+        /// <summary>
+        /// Read all data
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Read<T>() where T : new()
+        {
+            if (File.Exists(_filename))
+            {
+                using (var file = File.OpenText(_filename))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    return (T)serializer.Deserialize(file, typeof(T));
+                }
+            }
+
+            return new T();
+        }
+
+        /// <summary>
+        /// Save all data
+        /// </summary>
+        /// <typeparam name="T">Type of object to save</typeparam>
+        /// <param name="data">instance of object of type T that contains data to be written to disk</param>
+        public void Save<T>(T data)
+        {
+            JsonSerializerSettings serializerSettings = new JsonSerializerSettings();
+            string contents = JsonConvert.SerializeObject(data, (Formatting)1);
+            File.WriteAllText(_filename, contents);
+
+            //using (var file = File.CreateText(_filename))
+            //{
+            //    var serializer = new JsonSerializer();
+            //    serializer.Serialize(file, data);
+            //}
+        }
+
+        public void Delete()
+        {
+            File.Delete(_filename);
+        }
+    }
+}
